05360f217eec20885dd9a967c3076fb69a7318d0
imeya <m13951564301@163.com>
2021-07-05 16:02:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+import logging
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    print("hi ivan, this is print method...")
+    print("hello, this is a test log from print() method...")
+    logging.exception("exeption is from logging.exception() method...")
+    logging.info("info is from logging.info() method...")
     return "Hello, World!"
